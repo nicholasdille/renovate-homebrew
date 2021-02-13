5df54aef29d0fb37433121079cd69d32d1d94d84
Nicholas Dille <nicholas.dille@mailbox.org>
2021-02-13 16:09:04 +0100

    Added command

--- scripts/buildx.sh
+++ scripts/buildx.sh
@@ -3,3 +3,11 @@
 DATASOURCE=github-releases
 REPO=docker/buildx
 VERSION=0.5.1
+
+brew bump-formula-pr \
+    "--version=${VERSION}" \
+    --strict \
+    --no-browse \
+    --no-fork \
+    nicholasdille/tap/ytt-bin
+# Error: buildx-bin: the current URL requires specifying a `--revision=` argument.
