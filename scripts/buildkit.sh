7b0c55f313cbb2b82944c094b6463094ac2ee467
Nicholas Dille <nicholas.dille@mailbox.org>
2021-02-13 15:56:52 +0100

    Added command to update version

--- scripts/buildkit.sh
+++ scripts/buildkit.sh
@@ -3,3 +3,11 @@
 DATASOURCE=github-releases
 REPO=moby/buildkit
 VERSION=0.8.1
+
+brew bump-formula-pr nicholasdille/tap/buildkit-bin \
+    "--version=${VERSION}" \
+    --strict \
+    --no-browse \
+    --no-fork
+
+# Update resource SHA256
