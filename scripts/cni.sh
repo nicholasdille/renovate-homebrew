e48c819b9b17ca3e0bd1f90f2361ce7dde78a378
Nicholas Dille <nicholas.dille@mailbox.org>
2021-02-15 07:53:14 +0100

    Added bump command

--- scripts/cni.sh
+++ scripts/cni.sh
@@ -3,3 +3,11 @@
 DATASOURCE=github-releases
 REPO=containernetworking/plugins
 VERSION=0.9.1
+
+brew bump-formula-pr \
+    "--version=${VERSION}" \
+    --strict \
+    --no-browse \
+    --no-fork --write \
+    nicholasdille/tap/cni-bin
+# not a valid tar file
