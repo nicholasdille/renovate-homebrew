c86642d712cd6e5c5cfd816943538e692c2e2ace
Nicholas Dille <nicholas.dille@mailbox.org>
2021-02-15 08:56:34 +0100

    Removed test parameter

--- scripts/rootlesskit.sh
+++ scripts/rootlesskit.sh
@@ -8,12 +8,12 @@ brew bump-formula-pr \
     "--version=${VERSION}" \
     --strict \
     --no-browse \
-    --no-fork --write \
+    --no-fork \
     nicholasdille/tap/rootlesskit
 
 brew bump-formula-pr \
     "--version=${VERSION}" \
     --strict \
     --no-browse \
-    --no-fork --write \
+    --no-fork \
     nicholasdille/tap/rootlesskit-bin
