ee054a87e91a6cf541212e643c6bae06f8189de9
Nicholas Dille <nicholas.dille@mailbox.org>
2021-02-15 08:29:06 +0100

    Added bump command

--- scripts/clusterctl.sh
+++ scripts/clusterctl.sh
@@ -3,3 +3,17 @@
 DATASOURCE=github-releases
 REPO=kubernetes-sigs/cluster-api
 VERSION=0.3.14
+
+brew bump-formula-pr \
+    "--version=${VERSION}" \
+    --strict \
+    --no-browse \
+    --no-fork --write \
+    nicholasdille/tap/clusterctl
+
+brew bump-formula-pr \
+    "--version=${VERSION}" \
+    --strict \
+    --no-browse \
+    --no-fork --write \
+    nicholasdille/tap/clusterctl-bin
