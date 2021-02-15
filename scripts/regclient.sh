3749f28b8418cd65d97f9783c017ba195aff5461
Nicholas Dille <nicholas.dille@mailbox.org>
2021-02-15 08:43:26 +0100

    Added bump command

--- scripts/regclient.sh
+++ scripts/regclient.sh
@@ -3,3 +3,12 @@
 DATASOURCE=github-releases
 REPO=regclient/regclient
 VERSION=0.2.1
+
+brew bump-formula-pr \
+    "--version=${VERSION}" \
+    --strict \
+    --no-browse \
+    --no-fork --write \
+    nicholasdille/tap/regclient-bin
+
+# Update resource SHA256
