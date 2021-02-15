c59f6bf71617e92646ac471509c355a1f0660c8b
Nicholas Dille <nicholas.dille@mailbox.org>
2021-02-15 09:23:29 +0100

    Added bump command

--- scripts/kapp.sh
+++ scripts/kapp.sh
@@ -3,3 +3,10 @@
 DATASOURCE=github-releases
 REPO=vmware-tanzu/carvel-kapp
 VERSION=0.35.0
+
+brew bump-formula-pr \
+    "--version=${VERSION}" \
+    --strict \
+    --no-browse \
+    --no-fork \
+    nicholasdille/tap/kapp-bin
