a2af55bf3013708464fa3ebb1df9cfedd232208b
Nicholas Dille <nicholas.dille@mailbox.org>
2021-02-15 08:33:21 +0100

    Added bump command

--- scripts/clusterawsadm.sh
+++ scripts/clusterawsadm.sh
@@ -3,3 +3,17 @@
 DATASOURCE=github-releases
 REPO=kubernetes-sigs/cluster-api-provider-aws
 VERSION=0.6.4
+
+brew bump-formula-pr \
+    "--version=${VERSION}" \
+    --strict \
+    --no-browse \
+    --no-fork --write \
+    nicholasdille/tap/clusterawsadm
+
+brew bump-formula-pr \
+    "--version=${VERSION}" \
+    --strict \
+    --no-browse \
+    --no-fork --write \
+    nicholasdille/tap/clusterawsadm-bin
