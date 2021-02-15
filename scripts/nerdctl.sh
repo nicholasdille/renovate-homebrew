489a551b518cb0d35c1ae60d9339db00738834ed
Nicholas Dille <nicholas.dille@mailbox.org>
2021-02-15 07:44:02 +0100

    Added bump command

--- scripts/nerdctl.sh
+++ scripts/nerdctl.sh
@@ -3,3 +3,10 @@
 DATASOURCE=github-releases
 REPO=AkihiroSuda/nerdctl
 VERSION=0.5.0
+
+brew bump-formula-pr \
+    "--version=${VERSION}" \
+    --strict \
+    --no-browse \
+    --no-fork --write \
+    nicholasdille/tap/nerdctl-bin
