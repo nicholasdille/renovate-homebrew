90a7515a2cb62c1911a4e63be64d5e3825ca6e29
Nicholas Dille <nicholasdille@users.noreply.github.com>
2021-02-18 22:25:54 +0100

    Fixed syntax

--- scripts/buildkit.sh
+++ scripts/buildkit.sh
@@ -4,10 +4,9 @@ DATASOURCE=github-releases
 REPO=moby/buildkit
 VERSION=0.8.1
 
-brew bump-formula-pr nicholasdille/tap/buildkit-bin \
+brew bump-formula-pr \
     "--version=${VERSION}" \
     --strict \
     --no-browse \
-    --no-fork
-
-# Update resource SHA256
+    --no-fork \
+    nicholasdille/tap/buildkit-bin
