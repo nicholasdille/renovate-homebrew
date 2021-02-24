5fd27a69192cb4361b578f96395b9ecf15f5c8e9
Nicholas Dille <nicholas.dille@mailbox.org>
2021-02-24 14:45:46 +0100

    Added trivy

---
+++ scripts/trivy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DATASOURCE=github-releases
+REPO=aquasecurity/trivy
+VERSION=0.15.0
+
+brew bump-formula-pr \
+    "--version=${VERSION}" \
+    --strict \
+    --no-browse \
+    --no-fork \
+    nicholasdille/tap/trivy
