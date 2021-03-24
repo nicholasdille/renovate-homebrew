16a660081d91e8be78c43405f7a85869f00d230d
Nicholas Dille <nicholas.dille@mailbox.org>
2021-03-24 10:57:01 +0100

    Added vendir

---
+++ scripts/vendir.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DATASOURCE=github-releases
+REPO=vmware-tanzu/carvel-vendir
+VERSION=0.17.0
+
+brew bump-formula-pr \
+    "--version=${VERSION}" \
+    "$@" \
+    nicholasdille/tap/vendir
