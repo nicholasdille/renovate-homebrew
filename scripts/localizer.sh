a0f52f24286470b18c2694ff0e20d03685533141
Nicholas Dille <nicholas.dille@mailbox.org>
2021-04-13 14:11:05 +0200

    Added localizer

---
+++ scripts/localizer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DATASOURCE=github-releases
+REPO=getoutreach/localizer
+VERSION=1.8.2
+
+brew bump-formula-pr \
+    "--version=${VERSION}" \
+    "$@" \
+    nicholasdille/tap/localizer
