8d307be6248024be9b6193b1703464230e10c519
Nicholas Dille <nicholas.dille@mailbox.org>
2021-05-10 11:00:28 +0200

    Added bump for dockerd-rootless

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -8,24 +8,8 @@ brew bump-formula-pr \
     "--version=${VERSION}" \
     "$@" \
     nicholasdille/tap/dockerd
-    
-for FORMULA in dockerd; do
-    if test -f "Formula/${FORMULA}.rb"; then
-        echo "Updating SHA256 for formula ${FORMULA}"
 
-        URLS=$(grep -E "^\s+url" "Formula/${FORMULA}.rb" | sed -E "s/^\s+url\s\"(.+)\"$/\1/")
-        for URL in ${URLS}; do
-            echo "Processing ${URL}..."
-            SHA256=$(
-                curl --silent --location "${URL}" | \
-                    sha256sum | \
-                    cut -d' ' -f1
-            )
-            echo "Got SHA256 ${SHA256}"
-
-            mv "Formula/${FORMULA}.rb" "Formula/${FORMULA}.rb.bak"
-            cat "Formula/${FORMULA}.rb.bak" | tr '\n' '\r' | sed -E "s|(\s+)url \"${URL}\"\r(\s+)sha256 \"[0-9a-fA-F]+\"|\1url \"${URL}\"\r\2sha256 \"${SHA256}\"|" | tr '\r' '\n' >"Formula/${FORMULA}.rb"
-            rm "Formula/${FORMULA}.rb.bak"
-        done
-    fi
-done
+brew bump-formula-pr \
+    "--version=${VERSION}" \
+    "$@" \
+    nicholasdille/tap/dockerd-rootless
