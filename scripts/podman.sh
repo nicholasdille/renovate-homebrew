d1bb2327c36d9e798a7a3eaad9ac929cb4feb70a
Nicholas Dille <nicholas.dille@mailbox.org>
2021-06-06 22:11:14 +0200

    Updated formulae names

--- scripts/podman.sh
+++ scripts/podman.sh
@@ -4,7 +4,17 @@ DATASOURCE=github-releases
 REPO=containers/podman
 VERSION=3.2.0
 
+#brew bump-formula-pr \
+#    "--version=${VERSION}" \
+#    "$@" \
+#    nicholasdille/tap/podman
+
 brew bump-formula-pr \
     "--version=${VERSION}" \
     "$@" \
-    nicholasdille/tap/podman
+    nicholasdille/tap/podman-static
+
+brew bump-formula-pr \
+    "--version=${VERSION}" \
+    "$@" \
+    nicholasdille/tap/podman-remote
