66f8043b1acfed04991bb3e3452a3e65b2701c06
Nicholas Dille <nicholas.dille@mailbox.org>
2021-07-07 09:50:48 +0200

    Added dynamically linked podman

--- scripts/podman.sh
+++ scripts/podman.sh
@@ -4,10 +4,10 @@ DATASOURCE=github-releases
 REPO=containers/podman
 VERSION=3.2.1
 
-#brew bump-formula-pr \
-#    "--version=${VERSION}" \
-#    "$@" \
-#    nicholasdille/tap/podman
+brew bump-formula-pr \
+    "--version=${VERSION}" \
+    "$@" \
+    nicholasdille/tap/podman
 
 brew bump-formula-pr \
     "--version=${VERSION}" \
