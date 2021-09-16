ad6344466aea97afbed4b7cdb98f7ce57dc6f69c
Nicholas Dille <nicholas.dille@mailbox.org>
2021-09-16 09:32:29 +0200

    Regex requires three matches #286

--- scripts/docker-compose@1.sh
+++ scripts/docker-compose@1.sh
@@ -3,7 +3,7 @@
 DATASOURCE=github-releases
 REPO=docker/compose
 VERSION=1.29.2
-VERSIONING=regex:^1\.
+VERSIONING=regex:^(?<major>1)\.(?<minor>\d+)\.(?<patch>\d+)$
 
 brew bump-formula-pr \
     "--version=${VERSION}" \
