1c11ae3a7918ef2db2391551280a43a388b8fae2
Nicholas Dille <nicholas.dille@mailbox.org>
2021-10-04 11:52:24 +0200

    Added patat

---
+++ scripts/patat.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DATASOURCE=github-releases
+REPO=jaspervdj/patat
+VERSION=0.8.7.0
+
+brew bump-formula-pr \
+    "--version=${VERSION}" \
+    "$@" \
+    nicholasdille/tap/patat-bin
