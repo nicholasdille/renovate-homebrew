cc8304b841f8dfb6292ce9cc97fcac318d10ce5d
Nicholas Dille <nicholas.dille@mailbox.org>
2021-10-06 07:54:27 +0200

    Added patat

--- scripts/patat.sh
+++ scripts/patat.sh
@@ -7,4 +7,9 @@ VERSION=0.8.7.0
 brew bump-formula-pr \
     "--version=${VERSION}" \
     "$@" \
+    nicholasdille/tap/patat
+
+brew bump-formula-pr \
+    "--version=${VERSION}" \
+    "$@" \
     nicholasdille/tap/patat-bin
